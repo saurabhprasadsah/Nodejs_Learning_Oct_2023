c5fcec1cde29dfb3dce2ffef1f992282146058f3
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-11-08 20:59:13 +0530

    first commit

--- module.js
+++ module.js
@@ -2,13 +2,12 @@
 // const color = require('cli-color')
 
 // console.log(color.red("hello red color"));
-
 //IIFE FUNCTION(Immediately Invoked Function Expression)
+
 (function(name){
     var age = 25;
     var name = "Saurabh"
     console.log(name,age);
     
 })()
-
 //IIFE(invoke function)
