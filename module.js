7f91061245b1e9706371651eabb6d5c0e7099a6a
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-11-11 15:57:45 +0530

    first commit

--- module.js
+++ module.js
@@ -1,17 +1,14 @@
 
-// const color = require('cli-color')
+const color = require('cli-color')
 
-// console.log(color.red("Hello Red Color"));
+console.log(color.red("Hello Nodejs from javascript"));
+
 // IIFE FUNCTION(Immediately Invoked Function Expression)
-
-(function(name){
-
-    var age = 25;
-    var name = "Saurabh";
-    console.log(name,age);
-
-})()
-
+// (function(name){
+//     var age = 25;
+//     var name = "Saurabh";
+//     console.log(name,age);
+// })()
 
 //IIFE(invoke function)
 
